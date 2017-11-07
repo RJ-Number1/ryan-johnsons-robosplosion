c54d3f2ce9bf5c77f59f938bdb9fbde141b0bc1a
RJ-Number1 <33134010+RJ-Number1@users.noreply.github.com>
2017-11-07 15:17:30 -0600

    Working Sensors
    Setup and verified 6 VL53L0X sensors

--- robosplosion/robosplosion.ino
+++ robosplosion/robosplosion.ino
@@ -8,15 +8,12 @@
 #include <Wire.h>
 #include <VL53L0X.h>
 
-
-
-VL53L0X frontRight;
-VL53L0X frontLeft;
-VL53L0X leftFront;
 VL53L0X leftBack;
+VL53L0X leftFront;
+VL53L0X frontLeft;
+VL53L0X frontRight;
 VL53L0X rightFront;
-VL53L0X rightLeft;
-
+VL53L0X rightBack;
 
 #define xbeeComm Serial3
 DriveMotors myMotors(MOTOR_LEFT_CH0, MOTOR_LEFT_CH1,
@@ -27,21 +24,31 @@ Servo mallet;
 byte newByte;
 
 void setup() {
-Serial.begin(9600);
+  Serial.begin(9600);
   xbeeComm.begin(9600);
   mallet.attach(MALLET_PIN);
   mallet.write(MALLET_RETRACT_POS);
   while (! Serial) {
     delay(1);
   }
-   
+  Wire.begin();
+  
+  pinMode(SENSOR_LEFT_REAR,OUTPUT);
+  pinMode(SENSOR_LEFT_FRONT,OUTPUT);
+  pinMode(SENSOR_FRONT_LEFT,OUTPUT);
+  pinMode(SENSOR_FRONT_RIGHT,OUTPUT);
+  pinMode(SENSOR_RIGHT_FRONT,OUTPUT);
+  pinMode(SENSOR_RIGHT_REAR,OUTPUT);
+  
   digitalWrite(SENSOR_LEFT_REAR,LOW);
   digitalWrite(SENSOR_LEFT_FRONT,LOW);
   digitalWrite(SENSOR_FRONT_LEFT,LOW);
   digitalWrite(SENSOR_FRONT_RIGHT,LOW);
   digitalWrite(SENSOR_RIGHT_FRONT,LOW);
   digitalWrite(SENSOR_RIGHT_REAR,LOW);
+  
   delay(10);
+  
   digitalWrite(SENSOR_LEFT_REAR,HIGH);
   digitalWrite(SENSOR_LEFT_FRONT,HIGH);
   digitalWrite(SENSOR_FRONT_LEFT,HIGH);
@@ -49,19 +56,77 @@ Serial.begin(9600);
   digitalWrite(SENSOR_RIGHT_FRONT,HIGH);
   digitalWrite(SENSOR_RIGHT_REAR,HIGH);
   
-  digitalWrite(SENSOR_LEFT_REAR,HIGH);
+  digitalWrite(SENSOR_LEFT_REAR,LOW);
   digitalWrite(SENSOR_LEFT_FRONT,LOW);
   digitalWrite(SENSOR_FRONT_LEFT,LOW);
   digitalWrite(SENSOR_FRONT_RIGHT,LOW);
   digitalWrite(SENSOR_RIGHT_FRONT,LOW);
   digitalWrite(SENSOR_RIGHT_REAR,LOW);
   
-  frontRight.init(true);
-  frontRight.setAddress(0x50);
+  delay(10);
   
+  pinMode(SENSOR_LEFT_REAR, INPUT);
+  digitalWrite(SENSOR_LEFT_REAR,HIGH);
+  leftBack.init(true);
+  leftBack.setTimeout(500);
+  leftBack.setAddress(0x30);
+  
+  pinMode(SENSOR_LEFT_FRONT, INPUT);
+  //digitalWrite(SENSOR_LEFT_FRONT,HIGH);
+  leftFront.init(true);
+  leftFront.setTimeout(500);
+  leftFront.setAddress(0x31);
+  
+  pinMode(SENSOR_FRONT_LEFT, INPUT);
+  digitalWrite(SENSOR_FRONT_LEFT,HIGH);
+  frontLeft.init(true);
+  frontLeft.setTimeout(500);
+  frontLeft.setAddress(0x32);
+  
+  pinMode(SENSOR_FRONT_RIGHT, INPUT);
+  digitalWrite(SENSOR_FRONT_RIGHT,HIGH);
+  frontRight.init(true);
+  frontRight.setTimeout(500);
+  frontRight.setAddress(0x33);
+  
+  pinMode(SENSOR_RIGHT_FRONT, INPUT);
+  digitalWrite(SENSOR_RIGHT_FRONT,HIGH);
+  rightFront.init(true);
+  rightFront.setTimeout(500);
+  rightFront.setAddress(0x34);
+  
+  pinMode(SENSOR_RIGHT_REAR, INPUT);
+  digitalWrite(SENSOR_RIGHT_REAR,HIGH);
+  rightBack.init(true);
+  rightBack.setTimeout(500);
+  rightBack.setAddress(0x35);  
+
+  leftBack.startContinuous();
+  leftFront.startContinuous();
+  frontLeft.startContinuous();
+  frontRight.startContinuous();
+  rightFront.startContinuous();
+  rightBack.startContinuous();
 }
+
 void loop() {
-  Serial.print(frontRight.readRangeContinuousMillimeters());
+  Serial.print("LB - " + String(leftBack.readRangeContinuousMillimeters()) + " | ");
+  if (leftBack.timeoutOccurred()) { Serial.print(" TIMEOUT "); }
+  Serial.print("LF - " + String(leftFront.readRangeContinuousMillimeters()) + " | ");
+  if (leftFront.timeoutOccurred()) { Serial.print(" TIMEOUT "); }
+  Serial.print("FL - " + String(frontLeft.readRangeContinuousMillimeters()) + " | ");
+  if (frontLeft.timeoutOccurred()) { Serial.print(" TIMEOUT "); }
+  Serial.print("FR - " + String(frontRight.readRangeContinuousMillimeters()) + " | ");
+  if (frontRight.timeoutOccurred()) { Serial.print(" TIMEOUT "); }
+  Serial.print("RF - " + String(rightFront.readRangeContinuousMillimeters()) + " | ");
+  if (rightFront.timeoutOccurred()) { Serial.print(" TIMEOUT "); }
+  Serial.print("RB - " + String(rightBack.readRangeContinuousMillimeters()) + " | ");
+  if (rightBack.timeoutOccurred()) { Serial.print(" TIMEOUT "); }
+
+  Serial.println();
+  delay(100);
+//  Serial.print(frontRight.readRangeSingleMillimeters());
+//  Serial.println();
   newByte = xbeeComm.read();
 
   if (newByte != -1) {

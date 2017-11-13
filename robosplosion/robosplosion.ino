0353413e6110ec7e1fb1bdb20c13d232cc50feba
Bryce <b.derriso@gmail.com>
2017-11-13 10:29:39 -0600

    Place driving method calls back in main loop.
    Needed to take advantage of global flag variables to ensure that
    commands only needed to be given once, rather than contsantly.
    Simplified boolean controls by eliminating dead code branches.

--- robosplosion/robosplosion.ino
+++ robosplosion/robosplosion.ino
@@ -21,50 +21,52 @@ Sensors sensors;
 int leftSpeed = 25;
 int rightSpeed = 25;
 
-boolean run = false;
-boolean dist = false;
-boolean test = false;
 bool forward = true;
 double targetDist;
 double LeftDist;
 double RightDist;
 
-byte newByte;
-
 void setup() {
     Serial.begin(9600);
     xbeeComm.begin(9600);
     sensors.reset();
+
     pinMode(SENSOR_LEFT_REAR, INPUT);
     digitalWrite(SENSOR_LEFT_REAR, HIGH);
     leftBack.init(true);
     leftBack.setTimeout(500);
     leftBack.setAddress(0x30);
+
     pinMode(SENSOR_LEFT_FRONT, INPUT);
     digitalWrite(SENSOR_LEFT_FRONT, HIGH);
     leftFront.init(true);
     leftFront.setTimeout(500);
     leftFront.setAddress(0x31);
+
     pinMode(SENSOR_FRONT_LEFT, INPUT);
     digitalWrite(SENSOR_FRONT_LEFT, HIGH);
     frontLeft.init(true);
     frontLeft.setTimeout(500);
     frontLeft.setAddress(0x32);
+
     pinMode(SENSOR_FRONT_RIGHT, INPUT);
     digitalWrite(SENSOR_FRONT_RIGHT, HIGH);
     frontRight.init(true);
     frontRight.setTimeout(500);
     frontRight.setAddress(0x33);
+
     pinMode(SENSOR_RIGHT_FRONT, INPUT);
     digitalWrite(SENSOR_RIGHT_FRONT, HIGH);
     rightFront.init(true);
     rightFront.setTimeout(500);
     rightFront.setAddress(0x34);
+
     pinMode(SENSOR_RIGHT_REAR, INPUT);
     digitalWrite(SENSOR_RIGHT_REAR, HIGH);
     rightBack.init(true);
     rightBack.setTimeout(500);
     rightBack.setAddress(0x35);
+
     leftBack.startContinuous();
     leftFront.startContinuous();
     frontLeft.startContinuous();
@@ -220,8 +222,10 @@ void ryansAlgorithm () {
 
 }
 
+bool displayDistances = false;
+bool stopped = true;
 void readUserInput() {
-    newByte = xbeeComm.read();
+    byte newByte = xbeeComm.read();
     if (newByte != -1) {
         switch (newByte) {
             case '\r':
@@ -231,19 +235,15 @@ void readUserInput() {
 
             case 's':
                 Serial.println("Stop command");
-                myMotors.driveStop();
+                stopped = true;
                 break;
             case 'w':
                 xbeeComm.println("Drive forward");
-                javiersAlgorithm();
+                stopped = false;
                 break;
             case ' ':
-                xbeeComm.println("Print Distance");
-                printDistance();
-                break;
-            case 't':
-                xbeeComm.println("Print Distance");
-                ryansAlgorithm();
+                xbeeComm.println("Flip distance display.");
+                displayDistances = !displayDistances;
                 break;
             default:
                 Serial.print("Unknown command: ");
@@ -254,5 +254,14 @@ void readUserInput() {
 
 void loop() {
     readUserInput();
+
+    if (displayDistances) {
+        printDistance();
+    } else if (stopped) {
+        myMotors.driveStop();
+    } else if (!stopped) {
+//        javiersAlgorithm();
+        ryansAlgorithm();
+    }
 }
 

692a7d3c024ca1ecf8878cc3b24d241880b17c25
RJ-Number1 <33134010+RJ-Number1@users.noreply.github.com>
2017-11-12 21:15:12 -0600

    removed some pid code and unnecessary code

--- robosplosion/robosplosion.ino
+++ robosplosion/robosplosion.ino
@@ -28,17 +28,6 @@ double targetDist;
 double LeftDist;
 double RightDist;
 
-//PID variables
-//double leftWheelInput, rightWheelInput, leftWheelOutput, rightWheelOutput;
-//float Kp=1;
-//float Ki=0;
-//float Kd=0;
-//PID leftWheelPID(&leftWheelInput, &leftWheelOutput, &targetDist, Kp, Ki, Kd, DIRECT);  
-//PID rightWheelPID(&rightWheelInput, &rightWheelOutput, &targetDist, Kp, Ki, Kd, DIRECT);
-//const long serialPing = 500;
-//unsigned long now = 0;
-//unsigned long lastMessage = 0;
-
 byte newByte;
 void setup() {
   Serial.begin(9600);
@@ -80,60 +69,17 @@ void setup() {
   frontRight.startContinuous();
   rightFront.startContinuous();
   rightBack.startContinuous();
-
-//  leftWheelPID.SetMode(AUTOMATIC);
-//  leftWheelPID.SetSampleTime(1);
-//  rightWheelPID.SetMode(AUTOMATIC);
-//  rightWheelPID.SetSampleTime(1);  
-//  lastMessage = millis(); 
 }
-
 
 void loop() {
   newByte = xbeeComm.read();
+//  xbeeComm.println(newByte);
   int sensDriverFront = leftFront.readRangeSingleMillimeters();
   int sensDriverBack = leftBack.readRangeSingleMillimeters();
   int sensPassFront = rightFront.readRangeSingleMillimeters();
   int sensPassBack = rightBack.readRangeSingleMillimeters();
   int sensFrontLeft = frontLeft.readRangeSingleMillimeters();
   int sensFrontRight = frontRight.readRangeSingleMillimeters();
-//  now = millis(); 
-//  if(now - lastMessage > serialPing) {
-//    //Serial.println("ask for new gain");
-//    if (Serial.available() > 0) {
-//      for (int x = 0; x < 4; x++) {
-//        switch (x) {
-//        case 0:
-//          Kp = Serial.parseFloat();
-//          break;
-//        case 1:
-//          Ki = Serial.parseFloat();
-//          break;
-//        case 2:
-//          Kd = Serial.parseFloat();
-//          break;
-//        case 3:
-//          for (int y = Serial.available(); y == 0; y--) {
-//          Serial.read(); //Clear out any residual junk
-//          }
-//          break;
-//        }
-//      }
-//      Serial.print(" Kp,Ki,Kd = ");
-//      Serial.print(Kp);
-//      Serial.print(",");
-//      Serial.print(Ki);
-//      Serial.print(",");
-//      Serial.println(Kd); //Let us know what we just received
-//      leftWheelPID.SetTunings(Kp, Ki, Kd); //Set the PID gain constants and start running
-//      rightWheelPID.SetTunings(Kp, Ki, Kd); //Set the PID gain constants and start running
-//     }
-//     else {
-//      //Serial.println("not available");
-//     }
-//    lastMessage = now; //update the time stamp
-//  }
-  
   
  if(run){
   int newLeftSpeed;
@@ -188,60 +134,38 @@ void loop() {
     leftSpeed = 50;
     rightSpeed = 50;
   }
+  else if (sensDriverFront < 300 && sensPassFront < 300 && sensDriverBack < 300 && sensPassBack < 300) {
+    int totalSpace = sensDriverBack + sensPassBack;
+    targetDist = (float)totalSpace/2;
+    xbeeComm.println((String)"Using all 4 side sensors (TotalSpace " + totalSpace + " | TargetDist " + targetDist + ")");
+    float avgDriver = (sensDriverFront + sensDriverBack) / 2;
+    float avgPass = (sensPassFront + sensPassBack) /2;
+    leftSpeed = ((float)targetDist / (float)avgDriver) * baseSpeed;
+    rightSpeed = ((float)targetDist / (float)avgPass) * baseSpeed;
+  }
   else if ((sensDriverFront < 300) && (sensPassFront < 300)) {
-    delay(100);
     int totalSpace = sensDriverFront + sensPassFront;
     targetDist = (float)totalSpace/2; 
     xbeeComm.println((String)"Using both front sensors (TotalSpace " + totalSpace + " | TargetDist " + targetDist + ")");
-//    leftWheelInput = (float)sensDriverFront;
-//    rightWheelInput = (float)sensPassFront;
-//    leftWheelPID.Compute();
-//    rightWheelPID.Compute();
-//    xbeeComm.println((String)"PID input | Left: " + leftWheelInput + " | Right: " + rightWheelInput);
-//    xbeeComm.println((String)"PID output | Left: " + leftWheelOutput + " | Right: " + rightWheelOutput);
-//    leftSpeed = 150 + leftWheelOutput;
-//    rightSpeed = 150 + rightWheelOutput;
-//    leftSpeed = ((float)targetDist / (float)sensDriverFront) * baseSpeed;
-//    rightSpeed = ((float)targetDist / (float)sensPassFront) * baseSpeed;
+    leftSpeed = ((float)targetDist / (float)sensDriverFront) * baseSpeed;
+    rightSpeed = ((float)targetDist / (float)sensPassFront) * baseSpeed;
   }
   else if ((sensDriverBack < 300) && (sensPassBack < 300)) {
     int totalSpace = sensDriverBack + sensPassBack;
     targetDist = (float)totalSpace/2;
     xbeeComm.println((String)"Using both back sensors (TotalSpace " + totalSpace + " | TargetDist " + targetDist + ")");
-//    leftWheelInput = (float)sensDriverBack;
-//    rightWheelInput = (float)sensPassBack;
-//    leftWheelPID.Compute();
-//    rightWheelPID.Compute();
-//    xbeeComm.println((String)"PID input | Left: " + leftWheelInput + " | Right: " + rightWheelInput);
-//    xbeeComm.println((String)"PID output | Left: " + leftWheelOutput + " | Right: " + rightWheelOutput);
-//    leftSpeed = leftWheelOutput;
-//    rightSpeed = rightWheelOutput;
     leftSpeed = ((float)targetDist / (float)sensDriverBack) * baseSpeed;
     rightSpeed = ((float)targetDist / (float)sensPassBack) * baseSpeed;
   }
-//  else if ((sensDriverFront < 300 && sensDriverBack < 300) && (sensFrontLeft < 300 && sensFrontRight < 300)) {
-//    xbeeComm.println("Turning right");
-//    leftSpeed = baseSpeed;
-//    rightSpeed = 0;
-//  }
-//  else if ((sensPassFront < 300 && sensPassBack < 300) && (sensFrontLeft < 300 && sensFrontRight < 300)) {
-//    xbeeComm.println("Turning left");
-//    rightSpeed = baseSpeed;
-//    leftSpeed = 0;
-//  }
   else if (sensDriverFront < 300 || sensDriverBack < 300) {
     xbeeComm.println("No right wall, turning right");
     leftSpeed = baseSpeed;
     rightSpeed = 0;
-//    leftSpeed = ((float)targetDist / (float)sensDriverFront) * baseSpeed;
-//    rightSpeed = (baseSpeed * 2) - leftSpeed;
   }
   else if (sensPassFront < 300 || sensPassBack < 300) {
     xbeeComm.println("No left wall, turning left");
     rightSpeed = baseSpeed;
     leftSpeed = 0;
-//    rightSpeed = ((float)targetDist / (float)sensPassFront) * baseSpeed;
-//    leftSpeed = (baseSpeed * 2) - rightSpeed;
   }
   else if (((sensFrontLeft + sensFrontRight) / 2) > 150) {
     xbeeComm.println("There are no walls");
@@ -254,18 +178,18 @@ void loop() {
   
 
 
-//  if(rightSpeed > 250 || leftSpeed > 250) {
-//    float reduc = 0;
-//    if (rightSpeed > 250) {
-//      reduc = (float)250/(float)rightSpeed;
-//    }
-//    else {
-//      reduc = (float)250/(float)leftSpeed;
-//    }
-//    xbeeComm.println((String)"Reducing speed (RS " + rightSpeed + " | LS " + leftSpeed + " | Reduction " + reduc + ")");
-//    rightSpeed = reduc * rightSpeed;
-//    leftSpeed = reduc * leftSpeed;
-//  }
+  if(rightSpeed > 250 || leftSpeed > 250) {
+    float reduc = 0;
+    if (rightSpeed > 250) {
+      reduc = (float)250/(float)rightSpeed;
+    }
+    else {
+      reduc = (float)250/(float)leftSpeed;
+    }
+    xbeeComm.println((String)"Reducing speed (RS " + rightSpeed + " | LS " + leftSpeed + " | Reduction " + reduc + ")");
+    rightSpeed = reduc * rightSpeed;
+    leftSpeed = reduc * leftSpeed;
+  }
 
   if(forward) {
     xbeeComm.println((String)"LeftSpeed: " + leftSpeed + " | RightSpeed: " + rightSpeed);
@@ -314,45 +238,9 @@ void loop() {
         test = true;
         //myMotors.driveForward(200, 200);
         break;
-//      case 'a':
-//        Serial.println("Turn left");
-//        myMotors.turnLeft(TURN_SPEED);
-//        break;
-//
-//      case 'd':
-//        Serial.println("Turn right");
-//        myMotors.turnRight(TURN_SPEED);
-//        break;
-//
-//      case 'q':
-//        Serial.println("Pivot left");
-//        myMotors.pivotLeft(TURN_SPEED);
-//        break;
-//
-//      case 'e':
-//        Serial.println("Pivot right");
-//        myMotors.pivotRight(TURN_SPEED);
-//        break;
-//
-//      case 'x':
-//        Serial.println("Drive reverse");
-//        myMotors.driveReverse(DRIVE_REVERSE_SPEED);
-//        break;
-//
-//      case 'm':
-//        Serial.println("Measurement");
-//        byte distances[3];
-//        //distances[0] = (byte) pingFront.getDistanceInCm();
-//        
-//        xbeeComm.write(distances, sizeof(distances)/sizeof(byte));
-//
-//        break;
-//
-//     
-//
-//      default:
-//        Serial.print("Unknown command: ");
-//        Serial.println(newByte);
+      default:
+        Serial.print("Unknown command: ");
+        Serial.println(newByte);
     }
   }
 }
